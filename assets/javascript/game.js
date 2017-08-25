af70840ca48b0d42bf9003482a4beae7e428dc2d
Lina C <linaclark96@gmail.com>
2017-08-25 15:32:05 -0400

    Finished game. All javascript tested and working.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,50 @@
 var computerChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var wins = 0;
 var losses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+var guessedLetters = [];
+var randomChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+var pickLetter = function() {
+    return randomChoice;
+}
+var uGuessed = function() {
+    document.querySelector("#guesses-left").innerHTML = guessesLeft;
+}
+var whatsGuessed = function() {
+    document.querySelector("#guesses").innerHTML = guessedLetters.join(" ");
+}
+var newGame = function() {
+    guesses = 9;
+    guessesLeft = 9;
+    guessedLetters = [];
 
+    pickLetter();
+    uGuessed();
+    whatsGuessed();
+}
+
+pickLetter();
+uGuessed();
 
 document.onkeyup = function(event) {
-    var userGuess = string.fromCharCode(event.keyCode).toUpperCase();
-    console.log(userGuess);
-    var computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    guessesLeft--;
+    var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
 
+    guessedLetters.push(userGuess);
+    uGuessed();
+    whatsGuessed();
+
+    if (guessesLeft > 0) {
+        if (userGuess === pickLetter) {
+            wins++;
+            document.querySelector("wins").innerHTML() = wins;
+            newGame();
+        }
+    } else if (guessesLeft == 0) {
+        losses++;
+        document.querySelector('#losses').innerHTML = losses;
+        newGame();
+    }
 
 }
